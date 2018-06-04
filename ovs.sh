d24901e66b7bbcfe0de9dad9d5df272166aa003a
Mohammed Alshaboti <alshaboti.it@gmail.com>
2018-06-04 19:34:54 +1200

    Update ovs.sh

--- ovs.sh
+++ ovs.sh
@@ -7,28 +7,25 @@ ctl_ip=192.168.1.100
 #for faucet SDN controller
 ctl_port=6653 
 ports=$(seq 1 3)
+# keep first LAN port (port 0) connected to the linux bridge (port 5)
 uci set network.@switch_vlan[0].ports='0 5'
 
 for port in $ports
 do
 lan=$((port+1))
 vlan=$((port+2))
-
 #add new array item
 uci add network switch_vlan
-
 #[-1] means the last added index
 uci set network.@switch_vlan[-1].device='switch0'
 uci set network.@switch_vlan[-1].vlan=$vlan''
 uci set network.@switch_vlan[-1].vid=$vlan''
 uci set network.@switch_vlan[-1].ports=$port' 0t'
-
 #create interfce for each vlan
 p='lan'$lan
 uci set network.$p=interface
 uci set network.$p.proto='static'
 uci set network.$p.ifname='eth0.'$vlan
-
 done
 
 uci commit network
@@ -51,14 +48,14 @@ ovs-vsctl set Bridge $OVSLAN fail_mode=secure
 ovs-vsctl set-controller $OVSLAN tcp:$ctl_ip:$ctl_port
 
 # Add LAN port to Open vSwitch (ovslan)
-
 for port in $ports
 do 
 ovs-vsctl --may-exist add-port $OVSLAN eth0.$((port+2)) -- set interface eth0.$((port+2)) ofport_request=$port
 done 
 # add Wifi
 ovs-vsctl --may-exist add-port $OVSLAN wlan0 -- set interface wlan0 ofport_request=$((port+1))
-#to add eth0 (wan)
+
+#to add eth1 (wan)
 uci delete network.wan
 uci delete network.wan6
 uci commit network
